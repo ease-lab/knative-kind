8db3706da46c262a227994e6d41b9e0f2dc1c946
Bora M. Alper <bora@boramalper.org>
2021-07-20 14:51:28 +0300

    create node-registry
    Signed-off-by: Bora M. Alper <bora@boramalper.org>

--- 01-kind.sh
+++ 01-kind.sh
@@ -33,6 +33,15 @@ elif [ "$REPLY" == "N" ] || [ "$REPLY" == "n" ] || [ -z "$REPLY" ]; then
   exit 0
 fi
 
+# Create registry container unless it already exists
+# https://kind.sigs.k8s.io/docs/user/local-registry/#create-a-cluster-and-registry
+reg_name='node-registry'
+reg_port='5000'
+running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+if [ "${running}" != 'true' ]; then
+  docker run -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" registry:2
+fi
+
 KIND_CLUSTER=$(mktemp)
 cat <<EOF | kind create cluster --name ${CLUSTER_NAME} --config=-
 kind: Cluster
@@ -40,7 +49,30 @@ apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
   image: kindest/node:${K8S_VERSION}
+containerdConfigPatches:
+- |-
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
+    endpoint = ["http://${reg_name}:${reg_port}"]
 EOF
 echo "Waiting on cluster to be ready..."
 sleep 10
 kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n kube-system > /dev/null
+
+# Connect the registry to the cluster network (the network may already be connected)
+# https://kind.sigs.k8s.io/docs/user/local-registry/#create-a-cluster-and-registry
+docker network connect 'kind' "${reg_name}" || true
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+# https://kind.sigs.k8s.io/docs/user/local-registry/#create-a-cluster-and-registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
